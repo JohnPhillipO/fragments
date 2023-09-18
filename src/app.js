3db7ad88d8179570c224ebb10d375f3fa4525dc0
JohnPhillipO <jostiano@gmail.com>
2023-09-17 22:46:24 -0400

    Secured our express routes with passport

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ const pino = require('pino-http')({
   logger,
 });
 
+const passport = require('passport');
+const authenticate = require('./auth');
+
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
 
@@ -25,6 +28,9 @@ app.use(cors());
 
 // Use gzip/deflate compression middleware
 app.use(compression());
+
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
