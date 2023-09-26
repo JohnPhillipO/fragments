2af4ec0edf4aa17de3323cfedd208d99f49c9a52
JohnPhillipO <jostiano@gmail.com>
2023-09-26 16:58:11 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -38,13 +38,7 @@ app.use('/', require('./routes'));
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    error: {
-      message: 'not found',
-      code: 404,
-    },
-  });
+  res.status(404).json(createErrorResponse({ code: 404, message: 'not found' }));
 });
 
 // Add error-handling middleware to deal with anything else
@@ -60,7 +54,13 @@ app.use((err, req, res, next) => {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json(createErrorResponse(status, message));
+  res.status(status).json({
+    status: 'error',
+    error: {
+      message,
+      code: status,
+    },
+  });
 });
 
 // Export our `app` so we can access it in server.js
